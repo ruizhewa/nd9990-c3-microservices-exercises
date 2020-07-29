6c229750b869d0fc5483d6ab4800dac9d6ba3e1d
Ruizhe Wang <ruizhe@oben.com>
2020-07-28 17:57:16 -0700

    update host endpoints [ci skip]

--- project/udagram-frontend/src/environments/environment.prod.ts
+++ project/udagram-frontend/src/environments/environment.prod.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://a9a8b33144c954bb2ac41c4efb694179-774358736.us-west-2.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
